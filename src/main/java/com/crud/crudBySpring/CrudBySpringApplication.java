3a622261ec74cc5d4494caf6d97111b08b3265c1
AbdulNabi9966 <122974594+AbdulNabi9966@users.noreply.github.com>
2023-08-16 10:31:47 +0530

    Add files via upload

---
+++ src/main/java/com/crud/crudBySpring/CrudBySpringApplication.java
@@ -0,0 +1,13 @@
+package com.crud.crudBySpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBySpringApplication.class, args);
+	}
+
+}
